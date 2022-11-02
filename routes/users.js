1889ed2517c840e29dd9fe350ce1d83c5f12bb2f
carlk <carloskachuk@gmail.com>
2022-11-02 08:19:31 -0300

    feat: the user delete route has been created

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ router.get('/', userController.get);
 
 router.post('/', userController.createUser);
 
-router.put('/:id', userController.updateUser)
+router.put('/:id', userController.updateUser);
+
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router
