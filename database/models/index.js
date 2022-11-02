a69a48ff487dc7c64df0a0a069acc43618237a0c
Luca Spiritini <luca.spiritini@hotmail.com>
2022-11-02 19:13:28 -0300

    BUGFIX: adding type of category

--- database/models/index.js
+++ database/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.js')[env];
 const db = {};
-console.log(config)
+// console.log(config)
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -34,7 +34,7 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-console.log(db)
+// console.log(db)
 module.exports = db;
 
 //Comentario
