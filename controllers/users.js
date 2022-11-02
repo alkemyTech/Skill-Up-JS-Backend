28d48cb966d82125e160c760c4cec308708991c4
carlk <carloskachuk@gmail.com>
2022-11-02 08:16:19 -0300

    feat: the user create controller has been created but without implementation

--- controllers/users.js
+++ controllers/users.js
@@ -4,21 +4,23 @@ const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
 // example of a controller. First call the service, then build the controller method
-module.exports = {
-  get: catchAsync(async (req, res, next) => {
-    try {
-      const response = await User.findAll()
-      endpointResponse({
-        res,
-        message: 'Users retrieved successfully',
-        body: response,
-      })
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving users] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
-    }
-  }),
-}
+exports.get = catchAsync(async (req, res, next) => {
+  try {
+    const response = await User.findAll()
+    endpointResponse({
+      res,
+      message: 'Users retrieved successfully',
+      body: response,
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `[Error retrieving users] - [index - GET]: ${error.message}`,
+    )
+    next(httpError)
+  }
+})
+
+exports.createUser = catchAsync(async(req, res, next)=>{
+  endpointResponse({res, message: 'NOT IMPLEMENTED: This is an user update controller'})
+})
