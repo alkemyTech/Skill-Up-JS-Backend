225abf84ba0754b3dcdc77f294b0c21269cd5673
Alex0711 <alex.senger@hotmail.com>
2022-11-02 19:49:26 -0300

    eliminar este commit

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,20 @@ const {
    get,
 } = require('../controllers/users')
 
+const { models } = require('../libs/sequelize')
+
 const router = express.Router()
 
-router.get('/', get)
+router.get('/', async (req, res) => {
+  const rta = await models.User.create({
+    firstName: 'Alejandro',
+    lastName: 'Senger',
+    email: 'Alex.senger@hotmail.com',
+    password: 'unPassSeguro:)',
+    roleId: 1,
+  })
+  res.send(rta);
+})
 
 
 module.exports = router
