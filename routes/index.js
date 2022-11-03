cb81744025d8512c68f98b94c8b3eaa6ddf2bdcd
Leo <leo.echeni@gmail.com>
2022-11-03 01:54:22 -0300

    added roles folder/added roleRouter to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const userRouter = require('./users')
 const accountRouter = require('./accounts')
+const roleRouter = require('./roles');
 
 function routerApi(app) {
   const router = express.Router();
   router.get('/', (req, res) => {
-    res.send("Hola mundo")
+    res.send("Hola mundo");
   })
   app.use('', router);
 
   router.use('/user', userRouter);
-  router.use('/account', accountRouter)
+  router.use('/account', accountRouter);
+  router.use('/', roleRouter);
 }
 
 module.exports = routerApi;
