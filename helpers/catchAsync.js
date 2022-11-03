ca6de0574357fc7882ee4ae71f7f5ed0d9e097e7
julandrod <julian.andres.rodriguez@gmail.com>
2022-11-03 14:41:34 -0500

    Update package.json with bcrypt, http-status-codes, jsonwebtoken

--- helpers/catchAsync.js
+++ helpers/catchAsync.js
@@ -1,7 +1,13 @@
-const catchAsync = (fn) => (req, res, next) => {
-  fn(req, res, next).catch(next)
-}
+const catchAsync = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
 
 module.exports = {
   catchAsync,
-}
+};
