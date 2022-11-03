ac9cddafdf04b0acfa6197c90061fdb2dc26eaf2
Alex0711 <alex.senger@hotmail.com>
2022-11-03 17:41:40 -0300

    FIX: all models DEPRECATE: dockers service: node

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const userRouter = require('./users')
 const accountRouter = require('./accounts')
-const roleRouter = require('./roles');
+// const roleRouter = require('./roles');
 
 function routerApi(app) {
   const router = express.Router();
-  router.get('/', (req, res) => {
-    res.send("Hola mundo");
-  })
   app.use('', router);
 
   router.use('/user', userRouter);
   router.use('/account', accountRouter);
-  router.use('/role', roleRouter);
+  // router.use('/role', roleRouter);
 }
 
 module.exports = routerApi;
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,20 +1,43 @@
-const express = require('express')
+const express = require('express');
 
-<<<<<<< HEAD:routes/users/index.js
-=======
+const { models } = require('../../libs/sequelize');
 
-const { models } = require('../libs/sequelize')
+const newUser = {
+  firstName: 'Carlos',
+  lastName: 'Senger',
+  email: 'email@mail.com',
+  password: 'asdfdfdf',
+  roleId: 1
+}
 
->>>>>>> development:routes/users.js
-const router = express.Router()
+const newRole = {
+  name: 'client',
+  description: 'un cliente'
+}
+
+const newAccount = {
+  userId: '047c8790-40cf-4547-bf8a-97dcbd327ff7'
+}
+
+const newTransaction = {
+  amount: 50,
+  type: 'esto se borra',
+  accountId: '742d3398-db22-4ef9-a43d-2a5f23f8cc73',
+}
+
+const router = express.Router();
 
 router.get('/', async (req, res) => {
-   const rta = 'hola mundo';
-   res.send(rta);
- })
+  try {
+    // const rta3 = await models.Role.create(newRole);
+    // const rta4 = await models.User.create(newUser);
+    // const rta3 = await models.Account.create(newAccount);
+    const rta3 = await models.Transaction.create(newTransaction);
+    res.send(rta3);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-<<<<<<< HEAD:routes/users/index.js
-=======
 
->>>>>>> development:routes/users.js
-module.exports = router
+module.exports = router;
