ceb230a3fdcab3baeda8160759fd1c832a7b1c4c
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-03 09:20:27 -0500

    feat:validationSchema to add and update transactio

---
+++ schemas/createUpdateTransaction.js
@@ -0,0 +1,49 @@
+const createUpdateTransaction = {
+  amount: {
+    exists: { errorMessage: 'Amount is required' },
+    trim: true,
+    isDecimal: {
+      options: {
+        locale: 'en-US',
+      },
+      errorMessage:
+        'Invalid value. Only integer or decimal numbers separated by a point are accepted',
+    },
+  },
+  concept: {
+    exists: { errorMessage: 'Concept is required' },
+    trim: true,
+  },
+  date: {
+    exists: { errorMessage: 'Date date is required' },
+    trim: true,
+    isDate: {
+      format: 'YYYY-MM-DD',
+      errorMessage: 'Invalid date. Accepted format YYYYY-MM-DD',
+    },
+  },
+  type: {
+    exists: { errorMessage: 'Type is required' },
+    isIn: {
+      options: ['topup', 'payment'],
+      errorMessage: 'The allowed values are topup and payment',
+    },
+  },
+  accountId: {
+    exists: { errorMessage: 'Account id is required' },
+    trim: true,
+    isInt: { errorMessage: 'Account id must be a integer number' },
+    toInt: true,
+  },
+  toAccountId: {
+    exists: { errorMessage: 'toAccountId is required' },
+    trim: true,
+    isInt: { errorMessage: 'toAccountId must be a integer number' },
+    toInt: true,
+  },
+  userId: {
+    exists: { errorMessage: 'User id is required' },
+  },
+};
+
+module.exports = createUpdateTransaction;
