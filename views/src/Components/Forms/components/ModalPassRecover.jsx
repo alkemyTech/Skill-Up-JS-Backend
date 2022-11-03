6dc98d9cdc93eeecc5cbef8eaf80c00a95db49d2
JazminFeliu <jazminfeliu@gmail.com>
2022-11-03 13:22:50 -0300

    added views and modified docker-compose

---
+++ views/src/Components/Forms/components/ModalPassRecover.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//ejemplo de component
+
+export const ModalPassRecover = () => {
+  
+  return (
+    <>
+      <ContainerModal>
+           <h1>Soy un modal</h1>
+          </ContainerModal>
+
+    </>
+  );
+};
