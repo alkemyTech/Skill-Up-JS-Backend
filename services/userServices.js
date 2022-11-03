403e9e8a1179775c139b3f5ce200336b5527077c
carlk <carloskachuk@gmail.com>
2022-11-03 08:05:29 -0300

    feat: user update service and function to check if a user exists have been implemented

--- services/userServices.js
+++ services/userServices.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
+const { ErrorObject } = require('../helpers/error');
 
 const createUserService = async(conditions, body)=>{
     const salt = await bcrypt.genSalt(10);
@@ -20,7 +21,51 @@ const createUserService = async(conditions, body)=>{
     return {user, created}
 }
 
+const checkUserExistence = async(conditions)=>{
+  try{
+    const userExist = await User.findOne({
+      where: conditions
+    });
+    
+    if(userExist === null){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+  catch(err){
+    return new ErrorObject(err.message, 500, err);
+  }
+}
+
+const userUpdateService = async(conditions, body)=>{
+  try{
+    const checkUser = await checkUserExistence(conditions);
+    if(!checkUser){
+      return null;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHashed = await bcrypt.hash(body.password, salt);  
+
+    const userBody = {
+      ...body,
+      password: passwordHashed
+    }
+
+    User.update(userBody, {
+      where: conditions,
+    });
+
+    return userBody;
+  }
+  catch(err){
+    return new ErrorObject(err.message, 500, err);
+  }
+}
 
 module.exports = {
-  createUserService
+  createUserService,
+  userUpdateService
 }
