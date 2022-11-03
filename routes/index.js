aefd9c803f0181b5296755bd56874574dd7d6646
kyoh2014 <kyoh2014@gmail.com>
2022-11-02 22:50:22 -0300

    transctions route implementation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const usersRouter = require('./users')
+const transactionRouter = require('./transaction')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.use('/users', usersRouter)
+router.use('/transactions', transactionRouter)
+
 
 module.exports = router
