b24df801f5481af6ae87668cc75c24d01e784a0c
julandrod <julian.andres.rodriguez@gmail.com>
2022-11-03 09:48:45 -0500

    Add: util function createJWT and isTokenValid

---
+++ utils/createJwt.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const createJWT = ({ payload }) => {
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_DURATION,
+  });
+
+  return accessToken;
+};
+
+const isTokenValid = ({ token }) => {
+  jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { createJWT, isTokenValid };
