ae3310f4d7d1fbeb469f905fa6c6cb433ebe1b87
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-03 09:25:46 -0500

    feat:validation schema to create-update accounts

---
+++ schemas/createUpdateAccount.js
@@ -0,0 +1,37 @@
+const createUpdateAccountSchema = {
+  creationDate: {
+    exists: { errorMessage: 'Creation date is required' },
+    trim: true,
+    isDate: {
+      format: 'YYYY-MM-DD',
+      errorMessage: 'Invalid date. Accepted format YYYYY-MM-DD',
+    },
+  },
+  money: {
+    exists: { errorMessage: 'Money field is required' },
+    trim: true,
+    isDecimal: {
+      options: {
+        locale: 'en-US',
+      },
+      errorMessage:
+        'Invalid value. Only integer or decimal numbers separated by a point are accepted',
+    },
+  },
+  isBlocked: {
+    exists: { errorMessage: 'isBlocked field is required' },
+    trim: true,
+    isBoolean: {
+      options: {
+        loose: false,
+        errorMessage:
+          "Invalid value. Only 'false', 'true', '0' and '1' are accepted",
+      },
+    },
+  },
+  userId: {
+    exists: { errorMessage: 'User id is required' },
+  },
+};
+
+module.exports = createUpdateAccountSchema;
