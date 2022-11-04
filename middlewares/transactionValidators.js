a35c9bba566053f4e1de59bb68b2f84b58b5606d
manuel <manuelalbertoricardes@gmail.com>
2022-11-04 15:22:21 -0300

    add transactionValidator middleware

---
+++ middlewares/transactionValidators.js
@@ -0,0 +1,33 @@
+
+const validateTransaction = [
+    check('amount')
+    .exists()
+    .custom((value,{req}) => {
+        if(value <= 0){
+            throw new Error('Amount must be greater than 0')
+        }
+        return true
+    }),
+    check('description')
+    .exists()
+    .not()
+    .isEmpty(),
+    check('userId')
+    .exists()
+    .not()
+    .isEmpty(),
+    check('categoryId')
+    .exists()
+    .not()
+    .isEmpty(),
+    check('type')
+    .exists()
+    .not()
+    .isEmpty(),
+    (req, res, next) => {
+        validateResult(req, res, next)
+    }
+]
+
+
+module.exports = {validateTransaction}
--- routes/transactions.js
+++ routes/transactions.js
@@ -6,10 +6,11 @@ const {
   updateTransaction,
   deleteTransaction
 } = require('../controllers/transactions')
+const {validateTransaction} = require("../middlewares/transactionValidators")
 
 const router = express.Router()
 
-router.route('/').get(getTransactions).post(createTransaction)
+router.route('/').get(getTransactions).post(validateTransaction, createTransaction)
 
 router.route('/:id').get(getTransactionById).put(updateTransaction).delete(deleteTransaction)
 module.exports = router
