4f4926dab0259693a59b8528f4a7a13cd800b0f2
Leo <leo.echeni@gmail.com>
2022-11-04 20:42:01 -0300

    Components/Button: rehusable button component enabled

---
+++ views/src/Components/Buttons/Button.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+
+const Botons= ({ event, preset, value, type, payload}) => {
+    
+  
+    // events can be passed to this child component to dispatch an action with data and or payload, updating the store 
+    // in parent component we can set an useEffect to await for the results of the action or the updated store to re-render a specific DOM section with the request
+    return (
+        <> 
+            <button type={ type || null} className={preset || null} onClick={event? ()=> event(payload) : console.log("not passing a funcion on 'event' prop ")} > {value || "need a value here"}</button>
+        </>
+    )
+}
+
+export default Botons;
