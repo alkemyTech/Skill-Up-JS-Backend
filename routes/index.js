38061c8b49f99fa74f97a6fe125f20e649649e78
Alex0711 <alex.senger@hotmail.com>
2022-11-04 20:16:51 -0300

    ADD: error handlers, transactions:route, controler

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = require('./users')
 const accountRouter = require('./accounts')
 // const roleRouter = require('./roles');
+const transactionsRouter = require('./transactions')
 
 function routerApi(app) {
   const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi(app) {
   router.use('/user', userRouter);
   router.use('/account', accountRouter);
   // router.use('/role', roleRouter);
+  router.use('/transaction', transactionsRouter);
 }
 
 module.exports = routerApi;
