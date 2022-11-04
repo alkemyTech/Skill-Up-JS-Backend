b3f54c5771dca79f4ec672594aeef009602620bb
Leo <leo.echeni@gmail.com>
2022-11-04 02:24:03 -0300

    POST: /CreateUser store hassed password, remains creating an account

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,31 +1,42 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt')
 const { models } = require('../../libs/sequelize');
 
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
-  const { schema } = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
+  let { schema } = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
   // user as a account 
   // user as a roleId
+  const saltRounds = 10;
+
+
+
   try {
+
 
     // backlog says: - Deberá encriptar la contraseña en la base de datos con la librería bcrypt
     // so we need to implement a middleware on the DB to hash it OR a method inside the schema
     // I'm gonna do it here because I've never use sequelize's inyection in class way. tomorrow in the daily I'll be commenting this.
 
+    const pass = await bcrypt.hash(schema.password, saltRounds).then(function (hash) {
+      return hash;
+    });
 
+    // TODO: if not already exists, create an account
     const [row, created] = await models.User.findOrCreate({
       where: {
         email: schema.email
       },
       defaults: {
         ...schema,
+        password: pass,
         roleId: "1"
       }
     })
+
     if (created) return res.status(201).send(row);
-    else throw new Error("This email belongs to an existing account")
+    else throw new Error('This email belongs to an existing account')
 
   } catch (error) {
     res.status(400).send(error.message);
