e93d09b23fe6a1fa24e7deae660112f8f655b76a
Leo <leo.echeni@gmail.com>
2022-11-04 21:34:27 -0300

    REFACTOR: clean create user/account/role

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,48 @@
-const createHttpError = require('http-errors')
-const { User } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
-
+const { models } = require('../libs/sequelize');
+const ctrlAccount = require("./account");
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt')
 // example of a controller. First call the service, then build the controller method
 module.exports = {
-  get: catchAsync(async (req, res, next) => {
-    try {
-      const response = await User.findAll()
-      endpointResponse({
-        res,
-        message: 'Users retrieved successfully',
-        body: response,
-      })
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving users] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
+  get: async (id) => {
+    const user = await models.User.findOne({
+      where: {
+        id
+      },
+      include: ["role"]
+    });
+    const account = await models.Account.findOne({
+      where: {
+        userId: id
+      }
+    });
+
+    if (user && account) return { user, account };
+    else throw boom.notFound('User not found')
+  },
+  post: async (schema) => {
+
+    const saltRounds = 10;
+    const pass = await bcrypt.hash(schema.password, saltRounds).then(function (hash) {
+      return hash;
+    });
+    const [user, created] = await models.User.findOrCreate({
+      where: {
+        email: schema.email
+      },
+      defaults: {
+        ...schema,
+        password: pass
+      }
+    })
+    if (created) {
+      //here the axios.post with userId to accounts.post/create to link an account in a new user
+      let account = await ctrlAccount.post(user.id);
+      return { user, account } // and account;
     }
-  }),
+    else throw new Error('This email belongs to an existing account')
+
+
+  }
+
 }
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,63 +1,29 @@
 const express = require('express');
 const bcrypt = require('bcrypt')
 const { models } = require('../../libs/sequelize');
-
+const userController = require("../../controllers/users");
 const router = express.Router();
 
 
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const user = await models.User.findOne({
-      where: {
-        id
-      },
-      include: ["role"]
-    })
-    const account = await models.Account.findOne({
-      where: {
-        userId: id
-      }
-    })
-    if (user && account) res.send({ user, account })
-    else throw new Error("User not found")
+    let user = await userController.get(id);
+    return res.status(201).send(user);
   } catch (error) {
-    res.status(404).send(error.message)
+    next(error)
   }
-
-
 })
 
 router.post('/create', async (req, res) => {
-  // backlog says: - Deberá encriptar la contraseña en la base de datos con la librería bcrypt
-  // so we need to implement a middleware on the DB to hash it OR a method inside the schema
-  // I'm gonna do it here because I've never use sequelize's inyection in class way. tomorrow in the daily I'll be commenting this.
+
   let { schema } = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
   // user as a account
   // user as a roleId
   try {
-    const saltRounds = 10;
-    const pass = await bcrypt.hash(schema.password, saltRounds).then(function (hash) {
-      return hash;
-    });
-    // TODO: if not already exists, create an account
-    const [row, created] = await models.User.findOrCreate({
-      where: {
-        email: schema.email
-      },
-      defaults: {
-        ...schema,
-        password: pass
-      }
-    })
-    if (created) {
-      //here the axios.post with userId to accounts.post/create to link an account in a new user
-      // if needed also to fetch the description of the role
-      return res.status(201).send(row);
-    }
-    else throw new Error('This email belongs to an existing account')
-
+    let user = await userController.post(schema);
+    res.status(201).send(user);
   } catch (error) {
     res.status(400).send(error.message);
   }
