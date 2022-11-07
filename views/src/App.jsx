7e0382d722b0fa2fb447dccf0f7aaa4fb72ca02b
Leo <leo.echeni@gmail.com>
2022-11-07 20:39:38 -0300

    added userProfile template

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Layout from "./Components/Layout/Layout";
-
+import UserProfile from "./Components/User/UserProfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-      <Route index element={<Landing />}/>
+        <Route index element={<Landing />} />
+        <Route path="/user/profile/:id" element={<UserProfile />} />
       </Route>
     </Routes>
   );
