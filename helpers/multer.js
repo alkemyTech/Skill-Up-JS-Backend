c97da3a1679cf472fafc08f4a48841b71da6ee8c
mldamico <matias.lioneldamico@gmail.com>
2022-11-07 10:01:56 -0300

    Validation files

--- helpers/multer.js
+++ helpers/multer.js
@@ -5,10 +5,27 @@ const { v4: uuid } = require("uuid");
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../public/uploads"),
   filename: (req, file, cb) => {
-    cb(null, uuid() + path.extname(file.originalname));
+    const extension = path.extname(file.originalname);
+    if (extension !== "") cb(null, uuid() + path.extname(extension));
   },
 });
 
-const upload = multer({ storage });
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|svg|webp/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Only Images can be uploaded");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (_req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
 
 module.exports = { upload };
