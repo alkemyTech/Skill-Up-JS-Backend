b35f69a87de92a6b7133c845cd9730f17e37e756
Tomasb <tomasezequiel.benitez@gmail.com>
2022-11-07 16:06:28 -0300

    add component loader

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -3,12 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Layout from "./Components/Layout/Layout";
 
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-      <Route index element={<Landing />}/>
+        <Route index element={<Landing />} />
       </Route>
     </Routes>
   );
