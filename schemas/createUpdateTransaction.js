116f88f67124f037e7bbfc0566481dcec68e81f8
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 10:27:10 -0500

    refactor: add and delete properties

--- schemas/createUpdateTransaction.js
+++ schemas/createUpdateTransaction.js
@@ -1,52 +1,44 @@
 const createUpdateTransaction = {
   amount: {
-    exists: { errorMessage: 'Amount is required' },
+    exists: { bail: true, errorMessage: 'Amount is required' },
     trim: true,
     isFloat: {
       options: {
         locale: 'en-US',
+        min: 0.1,
       },
       errorMessage:
         'Invalid value. Only integer or decimal numbers separated by a point are accepted',
     },
   },
   concept: {
-    exists: { errorMessage: 'Concept is required' },
+    exists: { bail: true, errorMessage: 'Concept is required' },
     trim: true,
     isLength: {
       errorMessage: 'Concept has invalid length (max 25)',
-      options: { max: 25 },
+      options: { min: 0, max: 25 },
     },
   },
-  date: {
-    exists: { errorMessage: 'Date date is required' },
-    trim: true,
-    isDate: {
-      format: 'YYYY-MM-DD',
-      errorMessage: 'Invalid date. Accepted format YYYYY-MM-DD',
-    },
-  },
-  type: {
-    exists: { errorMessage: 'Type is required' },
+  category: {
+    exists: { bail: true, errorMessage: 'Category is required' },
     isIn: {
-      options: ['topup', 'payment'],
-      errorMessage: 'The allowed values are topup and payment',
+      options: ['Egress', 'Income'],
+      errorMessage: 'The allowed values are Egress and Income',
     },
   },
   accountId: {
-    exists: { errorMessage: 'Account id is required' },
+    exists: { bail: true, errorMessage: 'Account id is required' },
     trim: true,
-    isInt: { errorMessage: 'Account id must be a integer number' },
-    toInt: true,
+    isUUID: {
+      errorMessage: 'Account id invalid',
+    },
   },
   toAccountId: {
-    exists: { errorMessage: 'toAccountId is required' },
+    exists: { bail: true, errorMessage: 'toAccountId is required' },
     trim: true,
-    isInt: { errorMessage: 'toAccountId must be a integer number' },
-    toInt: true,
-  },
-  userId: {
-    exists: { errorMessage: 'User id is required' },
+    isUUID: {
+      errorMessage: 'Account id invalid',
+    },
   },
 };
 
