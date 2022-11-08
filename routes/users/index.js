72e9999a00973d7dcf6068e318aeced0d4dffd3b
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 14:55:48 -0500

    style: formatting

--- routes/users/index.js
+++ routes/users/index.js
@@ -49,7 +49,7 @@ router.get('/', authenticateUser, async (req, res, next) => {
 });
 
 router.post('/create', async (req, res, next) => {
-  let { schema } = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
+  let schema = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
   // user as a account
   // user as a roleId
   try {
