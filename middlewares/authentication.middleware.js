9c052be75a73fef73d5d69836c67a85bb1957a1e
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 09:26:09 -0500

    style: formatting

--- middlewares/authentication.middleware.js
+++ middlewares/authentication.middleware.js
@@ -1,22 +1,22 @@
-const { ErrorObject } = require("../helpers/error");
-const { StatusCodes } = require("http-status-codes");
-const { isTokenValid } = require("../utils/createJwt");
+const { ErrorObject } = require('../helpers/error');
+const { StatusCodes } = require('http-status-codes');
+const { isTokenValid } = require('../utils/createJwt');
 
 const authenticateUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new ErrorObject("No hay un token presente", StatusCodes.UNAUTHORIZED);
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new ErrorObject('No hay un token presente', StatusCodes.UNAUTHORIZED);
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   try {
     const payloadDecoded = isTokenValid({ token });
     req.user = { ...payloadDecoded };
     next();
   } catch (error) {
-    throw new ErrorObject("El token no es valido", StatusCodes.UNAUTHORIZED);
+    throw new ErrorObject('El token no es valido', StatusCodes.UNAUTHORIZED);
   }
 };
 
