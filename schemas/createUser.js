dd1a5aa476e05da3c57de6ed174285155ba37223
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 10:51:25 -0500

    refactor: refactor schema and rename

--- schemas/createUser.js
+++ schemas/createUser.js
@@ -1,14 +1,14 @@
 const createUpdateUserSchema = {
-  first_name: {
-    exists: { errorMessage: 'First name is required' },
+  firstName: {
+    exists: { bail: true, errorMessage: 'First name is required' },
     trim: true,
     isLength: {
       errorMessage: 'First name has invalid length (min 3, max 10)',
       options: { min: 3, max: 10 },
     },
   },
-  last_name: {
-    exists: { errorMessage: 'Last name is required' },
+  lastName: {
+    exists: { bail: true, errorMessage: 'Last name is required' },
     trim: true,
     isLength: {
       errorMessage: 'Last name has invalid length (min 3, max 10)',
@@ -16,24 +16,14 @@ const createUpdateUserSchema = {
     },
   },
   email: {
-    exists: {
-      errorMessage: 'Email is required',
-    },
+    exists: { bail: true, errorMessage: 'Email is required' },
     trim: true,
     isEmail: {
       errorMessage: 'Email is invalid',
     },
   },
-  roleId: {
-    exists: { errorMessage: 'Role id is required' },
-    trim: true,
-    isIn: {
-      options: ['1', '2'],
-      errorMessage: 'The allowed values for the role id are 1 and 2',
-    },
-  },
   password: {
-    exists: { errorMessage: 'Password is required' },
+    exists: { bail: true, errorMessage: 'Password is required' },
     trim: true,
     isStrongPassword: {
       minLength: 8,
@@ -44,14 +34,6 @@ const createUpdateUserSchema = {
     },
     errorMessage:
       'Password must be at least 8 characters and must include at least one upper case letter, one lower case letter, one numeric digit, and one special character.',
-  },
-  password_confirmation: {
-    exists: { errorMessage: 'Password confirmation is required' },
-    trim: true,
-    custom: {
-      options: (value, { req }) => value === req.body.password,
-      errorMessage: 'Passwords must match',
-    },
   },
 };
 
