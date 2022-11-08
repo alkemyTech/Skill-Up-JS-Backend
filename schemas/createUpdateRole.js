5ab09471d1c219b253688e7a89b9e49890695e2b
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 10:26:44 -0500

    refactor: add extra validations

--- schemas/createUpdateRole.js
+++ schemas/createUpdateRole.js
@@ -1,6 +1,6 @@
 const createUpdateRoleSchema = {
   name: {
-    exists: { errorMessage: 'Name is required' },
+    exists: { bail: true, errorMessage: 'Name is required' },
     trim: true,
     isLength: {
       errorMessage: 'Name has invalid length (min 2, max 12)',
@@ -8,7 +8,7 @@ const createUpdateRoleSchema = {
     },
   },
   description: {
-    exists: { errorMessage: 'Description is required' },
+    exists: { bail: true, errorMessage: 'Description is required' },
     trim: true,
     isLength: {
       errorMessage: 'Description has invalid length (min 3, max 50)',
