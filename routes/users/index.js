e9851f0c56b8498051f46a71468dc649fc3a0ba9
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-11-08 09:24:11 -0500

    style: formatting

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,41 +1,38 @@
 const express = require('express');
-const ctrlUser = require("../../controllers/users");
+const ctrlUser = require('../../controllers/users');
 const router = express.Router();
 
-
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
   try {
     let user = await ctrlUser.get(id);
     return res.status(201).send(user);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.post('/create', async (req, res, next) => {
-
-  let { schema } = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
+  let schema = req.body; // if the body will be validate by a middleware we already expect an object with valid info.
   // user as a account
   // user as a roleId
   try {
     let user = await ctrlUser.post(schema);
     res.status(201).send(user);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 router.delete('/:id', async (req, res, next) => {
-
   const { id } = req.params;
 
   try {
-    await ctrlUser.delete(id)
-    res.status(200).send("deleted")
+    await ctrlUser.delete(id);
+    res.status(200).send('deleted');
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 module.exports = router;
