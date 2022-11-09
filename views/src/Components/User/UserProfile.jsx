63de8d31cbd1f8a8b4b9147abdf6a78edd275686
Leo <leo.echeni@gmail.com>
2022-11-09 01:50:09 -0300

    add modal profile user

--- views/src/Components/User/UserProfile.jsx
+++ views/src/Components/User/UserProfile.jsx
@@ -1,17 +1,17 @@
 import {React, useState, useEffect, } from "react";
-import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers,updateUser } from "../../redux/features/users/usersGetSlice";
 import image from "../../../public/wallet.png";
 import Button from "../Buttons/Button";
 import Swal from "sweetalert2";
-const UserProfile = () => {
 
+const UserProfile = () => {
   
   const user = useSelector(state => state.users.usersList);
   const dispatch = useDispatch(); 
   const [stat, setStat] = useState();
-  const [editPass, setEditPass] = useState(  "Password: ")
+  const [editPass, setEditPass] = useState("Password: ");
+
   useEffect(() => {
     dispatch(getUsers());
   }, []);
@@ -27,40 +27,42 @@ const UserProfile = () => {
   
   const editProfile = () => {
     let inputs = document.querySelectorAll("input");
-    setEditPass("New password: ")
+    setEditPass("New password: ");
     setStat({
       ...stat,
       password: ""
-    })
-    
+    });
     let arrInputs = [...inputs]
     let str = "border-2 w-fit rounded p-2";
     arrInputs.map((e, i) => {
       if (e.name === "password") e.type = "text";
       if (e.name === "file") return e.classList.remove("hidden");
       e.disabled = false
-      if (e.name === "account") return e.disabled = true; 
+      if (e.name === "account") return e.disabled = true;
       e.classList.add(...str.split(" "));
-    })
-      
+    });
   } 
   const handleChange = (e) => {
-    
     setStat({
       ...stat,
       [e.target.name]: e.target.value
     }) 
   }
-  const fireSwal = () => {
-    console.log(user, "USER")
-    Swal.fire({
+  const fireSwal =async (type, payload, confirm= null,cancel, time=1500) => {
+    document.getElementById("pass").type="password"
+   let popup= await Swal.fire({
       position: "center",
-      icon: "success",
-      title: "Great! Changes has been saved",
-      showConfirmButton: false,
-      timer: 1500,
-      
-    });
+     icon: `${type}`,
+     showCancelButton: cancel || false,
+      title: `${payload}`,
+      showConfirmButton: `${confirm}`, 
+      timer: `${time}`,
+   });
+    if (popup.isConfirmed) return dispatch(updateUser(stat))
+    if (popup.dismiss === "cancel") return
+    if (popup.dismiss === "timer") return
+    window.location.reload()
+    return dispatch(updateUser(stat))
   };
   return (
     <div className="h-[80vh] flex justify-center ">
@@ -126,8 +128,10 @@ const UserProfile = () => {
           </label>
           <input
             type="password"
+            id="pass"
             className=" w-80 "
             value={stat?.password}
+            placeholder="Enter you new password"
             name="password"
             disabled={true}
           />
@@ -146,8 +150,8 @@ const UserProfile = () => {
         </div>
         <Button preset="mt-2 mb-2 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm  text-center  px-5 py-1.5 "
           type="submit" value="Save changes" event={() => {
-            dispatch(updateUser(stat))
-            fireSwal()
+            if(stat.password.length < 1) return fireSwal("warning","Password won't be changed",true,true,3500)
+           return fireSwal("success", "Great! Your changes as been saved.")
           }} /> 
       </form>
     </div>
