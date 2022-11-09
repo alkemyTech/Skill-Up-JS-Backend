97a8dc3e4a8534ab69ee81b085f2f2b6c18e229d
Tomasb <tomasezequiel.benitez@gmail.com>
2022-11-09 17:02:50 -0300

    fix formlogin

--- views/src/Components/Forms/FormLoginRegister.jsx
+++ views/src/Components/Forms/FormLoginRegister.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FormItem from "./components/FormItem";
 import { useAuth } from "../../hooks/useAuth";
 import { inputLogin, inputSchema } from "./components/validateSchema";
@@ -12,7 +12,7 @@ const FormLoginRegister = () => {
   const auth = useAuth();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
+
   const toggleLogin = () => {
     setIsLogin(!isLogin);
   };
@@ -27,8 +27,7 @@ const FormLoginRegister = () => {
         .catch((error) => {
           console.log(error);
         });
-         navigate("/dashboard")
-
+      navigate("/dashboard");
     } else {
       dispatch(createUser(values));
     }
@@ -40,7 +39,7 @@ const FormLoginRegister = () => {
     <section className="flex items-center justify-center min-h-[80vh] bg-gray-100">
       <Formik
         initialValues={{ name: "", lastName: "", email: "", password: "" }}
-        validationSchema={ isLogin ? inputLogin : inputSchema}
+        validationSchema={isLogin ? inputLogin : inputSchema}
         onSubmit={onSubmit}
         className="flex flex-row items-center justify-center lg:justify-start"
       >
@@ -83,7 +82,10 @@ const FormLoginRegister = () => {
               className="mt-2 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
             >
               {isLogin ? "Ingresar" : "Crear cuenta"}
-
+            </button>
+          </Form>
+        )}
+      </Formik>
     </section>
   );
 };
