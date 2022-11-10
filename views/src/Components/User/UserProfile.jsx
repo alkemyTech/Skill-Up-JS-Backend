d29fb9d85acfbb083a27de2eb1e1d94db1f38c23
Leo <leo.echeni@gmail.com>
2022-11-10 14:44:19 -0300

    refactor editProfile

--- views/src/Components/User/UserProfile.jsx
+++ views/src/Components/User/UserProfile.jsx
@@ -1,7 +1,6 @@
 import {React, useState, useEffect, } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsers,updateUser,deleteByUsers } from "../../redux/features/users/usersGetSlice";
-import image from "../../../public/wallet.png";
+import { getUsers,updateUser, } from "../../redux/features/users/usersGetSlice";
 import Button from "../Buttons/Button";
 import Swal from "sweetalert2";
 
@@ -11,7 +10,8 @@ const UserProfile = () => {
   const dispatch = useDispatch(); 
   const [stat, setStat] = useState();
   const [tooglePass, setTooglePass] = useState(false);
-  const[isEdited,setIsEdited]= useState(false)
+  const [isEdited, setIsEdited] = useState(false)
+  const [edit,setEdit]= useState(true)
   useEffect(() => {
     dispatch(getUsers());
   }, []);
@@ -27,24 +27,14 @@ const UserProfile = () => {
   },[user])
   
   const editProfile = () => {
-    let inputs = document.querySelectorAll("input");
-    setTooglePass(true);
+    setEdit(!edit)
+    setTooglePass(!tooglePass);
     setStat({
       ...stat,
       password: ""
     }); 
-    let arrInputs = [...inputs]
-    let str = "border-2 w-fit rounded p-2";
-    arrInputs.map((e, i) => {
-      if (e.name === "password") e.type = "text";
-      if (e.name === "file") return e.classList.remove("hidden");
-      e.disabled = false
-      if (e.name === "account") return e.disabled = true; 
-      e.classList.add(...str.split(" "));
-    });
   } 
   const handleChange = (e) => {
-    console.log("handleChange")
     setIsEdited(true)
     if (e.target.name === "file") {
       let supported = ["image/jpeg", "image/jpg", "image/svg+xml", "image/webp", "image/png"].map(img=> img=== e.target.files[0].type );
@@ -57,8 +47,8 @@ const UserProfile = () => {
           ...stat,
           image: formData
         });
-      } // add to stat to send the image for an update
-      else return // not send and include msg error of not supported
+      } 
+      else return 
     }
     setStat({
       ...stat,
@@ -66,7 +56,6 @@ const UserProfile = () => {
     }) 
   }
   const fireSwal = async (type, payload, confirm = null, cancel, time = 1500) => {
-     
     document.getElementById("pass").type="password"
    let popup= await Swal.fire({
       position: "center",
@@ -76,20 +65,19 @@ const UserProfile = () => {
       showConfirmButton: `${confirm}`, 
       timer: `${time}`,
    });
-     console.log(popup)
     if (popup.isConfirmed) {
       if (!isEdited) {
-        console.log("entre")
         setStat({ ...stat, password: user.password })
         return
       }
+      setIsEdited(false)
       return dispatch(updateUser(stat))
     }
     if (popup.dismiss) {
       isEdited && dispatch(updateUser(stat)) 
+      return setIsEdited(false)
     }
-
-
+    setIsEdited(false)
     return dispatch(updateUser(stat))
   }; 
  
@@ -100,7 +88,7 @@ const UserProfile = () => {
           <div className="sm:ml-20"></div>
                   <h1 className="text-2xl "> Your profile</h1>
           <Button preset="mt-2 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm  text-center  px-5 py-1.5 "
-            type="text" value="Edit" event={editProfile}  />
+            type="text" value={`${edit ? "Edit" : "Cancel" }`} event={editProfile}  />
         </div>
         <div className=" mb-0 mt-11  border-t-4 border-2 w-full h-1/6 flex items-center justify-between">
           <div className="h-full w-48 bg-gray-100 flex flex-col">
@@ -110,7 +98,7 @@ const UserProfile = () => {
               alt="Profile_image"
             />
           </div>
-          <input type="file" className=" w-80 hidden ml-2" name="file" />
+          <input type="file" className={`w-80  ml-2 ${edit? "hidden": "block"} `} name="file" />
           <div className=" md:ml-20"></div>
         </div>
         <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center">
@@ -119,10 +107,10 @@ const UserProfile = () => {
           </label>
         <input
             type="text"
-            className="  w-80 " 
+            className="  w-80 border-2 w-fit rounded p-2" 
            value={stat?.firstName}
             name="firstName"
-            disabled={true}
+            disabled={edit}
           />
         </div>
         <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center ">
@@ -130,25 +118,23 @@ const UserProfile = () => {
             Last name:
           </label>
           <input
-            
             type="text"
-            className=" w-80"
+            className=" w-80 border-2 w-fit rounded p-2"
             value={stat?.lastName}
             name="lastName"
-            disabled={true}
+            disabled={edit}
           />
         </div>
-
         <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center">
           <label className="pr-10   w-60" htmlFor="email">
             Email:
           </label>
           <input
             type="text"
-            className=" w-80 "
+            className=" w-80 border-2 w-fit rounded p-2"
             value={stat?.email}
             name="email"
-            disabled={true}
+            disabled={edit}
           />
         </div>
         <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center">
@@ -158,11 +144,11 @@ const UserProfile = () => {
           <input
             type="password"
             id="pass"
-            className=" w-80 "
-            value={stat?.password}
+            className=" w-80 border-2 w-fit rounded p-2"
+            value={edit ? user.password : stat.password}
             placeholder="Enter you new password"
             name="password"
-            disabled={true}
+            disabled={edit}
           />
           <h2 className="md: ml-6 md:block hidden">We value your safety. It is and will be encrypted!</h2>
         </div>
@@ -172,22 +158,18 @@ const UserProfile = () => {
           </label>
           <input
             type="text"
-            className=" w-80 "
-            value={user?.account?.money}
+            className=" w-80  "
+            value={`$ ${user?.account?.money}`}
             name="account"
             disabled={true}
           />
         </div>
-
           <Button preset="mt-2 mb-2 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm  text-center  px-5 py-1.5 "
           type="submit" value="Save changes" event={() => { 
             if (stat.password.length < 1) return fireSwal("warning", "Password won't be changed", true, true, 3500)
             if (isEdited) return fireSwal("success", "Great! Your changes as been saved.")
             return fireSwal("warning", "Nothing to change over here...", false,false, 3000)
             }} /> 
-
-        
-    
       </form>
     </div>
   );
