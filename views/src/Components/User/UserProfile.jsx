34688c9903a97e4f43a90dd5369b8e44a3cb4309
Leo <leo.echeni@gmail.com>
2022-11-10 19:42:27 -0300

    refactor fire swal and password input

--- views/src/Components/User/UserProfile.jsx
+++ views/src/Components/User/UserProfile.jsx
@@ -9,9 +9,9 @@ const UserProfile = () => {
   const user = useSelector(state => state.users.usersList);
   const dispatch = useDispatch(); 
   const [stat, setStat] = useState();
-  const [tooglePass, setTooglePass] = useState(false);
   const [isEdited, setIsEdited] = useState(false)
-  const [edit,setEdit]= useState(true)
+  const [edit, setEdit] = useState(true)
+  
   useEffect(() => {
     dispatch(getUsers());
   }, []);
@@ -21,18 +21,14 @@ const UserProfile = () => {
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
-      password: user.password,
+      password: "",
       image: user.image
     })
   },[user])
   
   const editProfile = () => {
     setEdit(!edit)
-    setTooglePass(!tooglePass);
-    setStat({
-      ...stat,
-      password: ""
-    }); 
+   
   } 
   const handleChange = (e) => {
     setIsEdited(true)
@@ -56,8 +52,8 @@ const UserProfile = () => {
     }) 
   }
   const fireSwal = async (type, payload, confirm = null, cancel, time = 1500) => {
-    document.getElementById("pass").type="password"
-   let popup= await Swal.fire({
+
+     await Swal.fire({
       position: "center",
      icon: `${type}`,
      showCancelButton: cancel || false,
@@ -65,22 +61,10 @@ const UserProfile = () => {
       showConfirmButton: `${confirm}`, 
       timer: `${time}`,
    });
-    if (popup.isConfirmed) {
-      if (!isEdited) {
-        setStat({ ...stat, password: user.password })
-        return
-      }
-      setIsEdited(false)
-      return dispatch(updateUser(stat))
-    }
-    if (popup.dismiss) {
-      isEdited && dispatch(updateUser(stat)) 
-      return setIsEdited(false)
-    }
-    setIsEdited(false)
-    return dispatch(updateUser(stat))
-  }; 
  
+    isEdited && dispatch(updateUser(stat))
+
+  };  
   return (
     <div className="h-[80vh] flex justify-center ">
      <form className="h-full w-[80vw]  flex flex-col  items-center border-2" onChange={(e)=> handleChange(e)}>
@@ -137,22 +121,8 @@ const UserProfile = () => {
             disabled={edit}
           />
         </div>
-        <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center">
-          <label className="pr-3 w-60" htmlFor="password" name="pass">
-           {tooglePass ? "New password:" : "Password:"}
-          </label>
-          <input
-            type="password"
-            id="pass"
-            className=" w-80 border-2 w-fit rounded p-2"
-            value={edit ? user.password : stat.password}
-            placeholder="Enter you new password"
-            name="password"
-            disabled={edit}
-          />
-          <h2 className="md: ml-6 md:block hidden">We value your safety. It is and will be encrypted!</h2>
-        </div>
-        <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center">
+       
+        <div className="mb-0 border-2 px-10 w-full h-1/6 flex items-center ">
           <label className="pr-4 md:pr-10 w-60" htmlFor="account">
             Account:
           </label>
@@ -164,15 +134,29 @@ const UserProfile = () => {
             disabled={true}
           />
         </div>
+        <div className={`mb-0 border-2 px-10 w-full h-1/6 flex items-center ${edit && "hidden"}`}>
+          <label className="pr-3 w-60" htmlFor="password" name="pass"> New password: 
+          </label>
+          <input
+            type="text"
+            id="pass"
+            className=" w-80 border-2 w-fit rounded p-2"
+            value={stat?.password}
+            placeholder="Enter you new password"
+            name="password"
+            disabled={edit}
+          />
+        </div>
           <Button preset="mt-2 mb-2 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm  text-center  px-5 py-1.5 "
           type="submit" value="Save changes" event={() => { 
             if (stat.password.length < 1) return fireSwal("warning", "Password won't be changed", true, true, 3500)
             if (isEdited) return fireSwal("success", "Great! Your changes as been saved.")
-            return fireSwal("warning", "Nothing to change over here...", false,false, 3000)
+            if(!isEdited) fireSwal("warning", "Nothing to change over here...", false,false, 3000) 
             }} /> 
-      </form>
+      </form> 
     </div>
   );
 };
 
 export default UserProfile;
+
