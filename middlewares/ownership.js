5f85eb0efb57ebf284a72c5bc1a18fe4ff1fd741
Juan <juanm.fernandez@hotmail.com>
2022-11-11 20:37:43 -0300

    FIX validation admin ownership

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -2,10 +2,11 @@ const { ErrorObject } = require('../helpers/error');
 
 const ownership = async (req, res, next) => {
   try {
+    
     const { id: paramId } = req.params;
     const { id, roleId } = req.user;
 
-    if (roleId === 1 || Number(paramId) === id) {
+    if (Number(roleId) === 1 || Number(paramId) === id) {
       return next();
     } else {
       res.status(403).send('You do not have the necessary permissions');
