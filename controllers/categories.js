6ba73350b44d6bd99e62e34a6233ca268b1194d1
lichaa824 <lisandrorubi@gmail.com>
2022-11-11 13:26:24 -0300

    FIX: Category endpoint now retrieves a response

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,67 +1,119 @@
-const { Category } = require('../database/models')
+const { Category } = require("../database/models");
+const { endpointResponse } = require("../helpers/success");
+const createHttpError = require("http-errors");
 
-const createCategory = async function(name, description){
-    try{
-    var cate = await Category.create({
-            name: name,
-            description: description
-        })
-      return cate}
-    catch(e){
-        console.log(e.message)
-        res.json(e.message)
+const createCategory = async function (req, res, next) {
+  try {
+    const response = await Category.create({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    if (response) {
+      endpointResponse({
+        res,
+        message: "Operacion exitosa",
+        body: response,
+      });
     }
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `Error creating category - ${error.message}`
+    );
+    next(httpError);
   }
+};
 
-const getCategories = async function(){
-    try{
-    return await Category.findAll(
-    )}
-    catch(e){
-        console.log(e.message)
-        res.json(e.message)
+const getCategories = async function (req, res, next) {
+  try {
+    const response = await Category.findAll();
+    if (response) {
+      endpointResponse({
+        res,
+        message: "Operacion exitosa",
+        body: response,
+      });
     }
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `Error retrieving all categories - ${error.message}`
+    );
+    next(httpError);
   }
+};
 
-  const getCategoryById = async function(id){
-    try{
-      var cat= await Category.findByPk(id)
-    return (cat)
-    }catch(e){
-          console.log(e.message)
-          res.json(e.message)
+const getCategoryById = async function (req, res, next) {
+  try {
+    const response = await Category.findByPk(req.params.id);
+    if (response) {
+      endpointResponse({
+        res,
+        message: "Operacion exitosa",
+        body: response,
+      });
+    } else {
+      res.status(404).json({ error: "Category not found", status: 404 });
+    }
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `Error retrieving category by id - ${error.message}`
+    );
+    next(httpError);
+  }
+};
+
+const modifyCategory = async function (req, res, next) {
+  try {
+    const response = await Category.update(
+      {
+        name: req.body.name,
+        description: req.body.description,
+      },
+      {
+        where: { id: req.params.id },
       }
+    );
+    if (response) {
+      endpointResponse({
+        res,
+        message: "Operacion exitosa",
+        body: response,
+      });
+    }
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `Error modifying category - ${error.message}`
+    );
+    next(httpError);
   }
+};
 
-  const modifyCategory = async function(id, name, description){
-    try{
-        await Category.update(
-            {
-            name: name,
-            description: description
-            },
-            {
-            where: { id: id },
-            }
-        );
-        res.send("Category updated");
-        }
-        catch(e){
-            console.log(e.message)
-            res.json(e.message)
-        }
+const deleteCategory = async function (req, res, next) {
+  try {
+    const response = await Category.destroy({ where: { id: req.params.id } });
+    if (response) {
+      endpointResponse({
+        res,
+        message: "Operacion exitosa",
+        body: response,
+      });
     }
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `Error retrieving deleting category - ${error.message}`
+    );
+    next(httpError);
+  }
+};
 
-  const deleteCategory = async function(id){
-    try{
-        var category = await Category.findByPk(id)
-        await category.destroy()
-        res.send("Category deleted")
-    }
-    catch(e){
-        console.log(e.message)
-        res.json(e.message)
-    }
-}
-
-module.exports = {createCategory, getCategories, getCategoryById, modifyCategory, deleteCategory}
+module.exports = {
+  createCategory,
+  getCategories,
+  getCategoryById,
+  modifyCategory,
+  deleteCategory,
+};
