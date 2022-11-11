260881760f98e7d5de2066e8cf31c45ac4d3e0ac
julandrod <julian.andres.rodriguez@gmail.com>
2022-11-11 10:18:06 -0500

    add: container for the form login

---
+++ views/src/Components/Forms/FormLoginContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FooterContainer from "../Footer/FooterContainer";
+import HeaderContainer from "../Header/HeaderContainer";
+import FormLoginRegister from "./FormLoginRegister";
+//ejemplo de component
+
+export const FormLoginContainer = () => {
+  return (
+    <>
+      <FormLoginRegister />
+    </>
+  );
+};
