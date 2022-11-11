32a221207d1e66120805b472e21687c9bc4592b0
lichaa824 <lisandrorubi@gmail.com>
2022-11-11 13:25:39 -0300

    FIX: Category endpoint routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,63 +1,22 @@
-const express = require('express')
+const express = require("express");
 const {
-   createCategory, getCategories, getCategoryById, modifyCategory, deleteCategory
-} = require('../controllers/categories')
+  createCategory,
+  getCategories,
+  getCategoryById,
+  modifyCategory,
+  deleteCategory,
+} = require("../controllers/categories");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/", async function(req, res){
-    let {name, description} = req.body
-    try{
-        var cat = await createCategory(name, description )
-        res.send("Category created")
-    }
-    catch(e){
-        res.send(e.message)
-    }
-})
+router.post("/", createCategory);
 
-router.get("/", async function(req, res){
-    try{
-        var cat = await getCategories()
-        res.json(cat)
-    }
-    catch(e){
-        res.send(e.message)
-    }
-})
+router.get("/", getCategories);
 
-router.get("/:id", async function(req, res){
-    let {id}=req.params
-    try{
-        var cat = await getCategoryById(id)
-        res.json(cat)
-    }
-    catch(e){
-        res.send(e.message)
-    }
-})
+router.get("/:id", getCategoryById);
 
-router.put("/:id", async function(req,res){
-    let{id}=req.params
-    let {name, description} = req.body
-    try{
-        var cat = await modifyCategory(id, name, description)
-        res.send("Category updated")
-    }
-    catch(e){
-        res.send(e.message)
-    }
-})
+router.put("/:id", modifyCategory);
 
-router.delete("/:id", async function(req, res){
-    let{id}=req.params
-    try{
-        var cat = await deleteCategory(id)
-        res.send("Category deleted")
-    }
-    catch(e){
-        res.send(e.message)
-    }
-})
+router.delete("/:id", deleteCategory);
 
-module.exports = router
+module.exports = router;
