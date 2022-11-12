73b47a899b824bb50624cb6cfe1bce0c5013da3b
mldamico <matias.lioneldamico@gmail.com>
2022-11-11 22:01:46 -0300

    fix category model

--- database/models/category.js
+++ database/models/category.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     /**
@@ -10,19 +8,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Category.hasMany(models.Transaction,{foreignKey: "transactionId", onDelete: 'cascade', onUpdate: 'cascade'})
+      Category.hasMany(models.Transaction);
     }
-  };
-  Category.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    deletedAt: {
-      type: DataTypes.STRING
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      deletedAt: {
+        type: DataTypes.STRING,
+      },
     },
-  }, {
-    sequelize,
-    timestamps: true,
-    modelName: 'Category',
-  });
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Category",
+    }
+  );
   return Category;
 };
