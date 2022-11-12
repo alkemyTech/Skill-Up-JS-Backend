6efae8dd07fa703bae3d880bc5b281216d2a33ad
Behelit0906 <104780873+Behelit0906@users.noreply.github.com>
2022-11-12 10:05:58 -0500

    refactor: change password validation (#68)

--- schemas/login.js
+++ schemas/login.js
@@ -10,14 +10,9 @@ const loginSchema = {
     exists: { bail: true, errorMessage: 'Password is required' },
     trim: true,
     isStrongPassword: {
-      minLength: 8,
-      maxLength: 15,
-      minLowecase: 1,
-      minUppercase: 1,
-      minSymbols: 1,
+      minLength: 1,
     },
-    errorMessage:
-      'Password must be at least 8 characters and must include at least one upper case letter, one lower case letter, one numeric digit, and one special character.',
+    errorMessage: 'Password must be at least 1 characters',
   },
 };
 
